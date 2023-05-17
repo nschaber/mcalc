0806c9cd75a4370989eab358881af98f2b9eb93b
Nicolas Schaber <nicolas.schaber@smateso.com>
2023-05-18 01:27:49 +0200

    Cleanup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,24 @@
-import {Fragment} from 'react'
+import {Fragment, useEffect, useState} from 'react'
 import {Outlet} from "react-router-dom";
 import MenuBar from "../components/MenuBar.tsx";
 
-const App = () => (
-    <Fragment>
-        <MenuBar/>
-        <Outlet/>
-    </Fragment>
-);
+const App = () => {
+
+    const [, setSplash] = useState<boolean>(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSplash(false);
+        }, 1000)
+    }, []);
+
+    return (
+        <Fragment>
+            <MenuBar/>
+            <Outlet/>
+        </Fragment>
+    )
+
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';

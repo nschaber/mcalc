97fe71ca8d550fa1a44862cd099ebd8f806ff886
Nicolas Schaber <nicolas.schaber@smateso.com>
2023-05-18 01:06:17 +0200

    Revert "Add icons"
    This reverts commit cde927f23d6e2b66aee1ddc2f8cca590498260b8.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {VitePWA} from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {port: 3000, open: true},
-    build: {outDir: "build"},
-    plugins: [react(), VitePWA({
-            manifest: {
-              name: "MCalc",
-              short_name: "MCalc",
-              theme_color: "#0296ff",
-            },
-            registerType: 'autoUpdate',
-
-        }
-    )],
+  server:{port: 3000, open: true},
+  build: {outDir: "build"},
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
 })
